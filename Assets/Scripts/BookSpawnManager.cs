177b6ef0c86afd1c8eebb792ba74107b3ca374a6
Jangmanbo <wkddpdnjs99@gmail.com>
2021-11-23 02:41:39 +0900

    update : BookSpawnManager (Delete the book every day and spawn it.)

--- Assets/Scripts/BookSpawnManager.cs
+++ Assets/Scripts/BookSpawnManager.cs
@@ -6,6 +6,7 @@ public class BookSpawnManager : MonoBehaviour
 {
     // J : https://angliss.cc/random-gameobject-created/ 참조
     public GameObject book;
+    public GameManager gameManager;
 
     int count = 3;                  // J : 찍어낼 책 개수
     private BoxCollider2D area;     // J : 박스 콜라이더의 사이즈 가져오기 위한 변수
@@ -16,23 +17,30 @@ public class BookSpawnManager : MonoBehaviour
     {
         area = GetComponent<BoxCollider2D>();
 
-        for (int i = 0; i < count; i++)// J : count만큼 생성
-        {
-            Spawn();    // J: 생성 + 스폰위치를 포함하는 함수
-        }
-
-        area.enabled = false;
+        StartCoroutine("Spawn");
     }
 
     // J : 게임 오브젝트를 복제하여 scene에 추가
-    private void Spawn()
+    private IEnumerator Spawn()
     {
-        Vector3 spawnPos = GetRandomPosition(); // J :랜덤 위치 return
+        for (int i = 0; i < count; i++) // J : count만큼 책 생성
+        {
+            Vector3 spawnPos = GetRandomPosition(); // J :랜덤 위치 return
 
-        // J : 원본, 위치, 회전값을 매개변수로 받아 오브젝트 복제
-        // J : Quaternion.identity <- 회전값 0
-        GameObject instance = Instantiate(book, spawnPos, Quaternion.identity);
-        bookList.Add(instance); // J : 오브젝트 관리를 위해 리스트에 add
+            // J : 원본, 위치, 회전값을 매개변수로 받아 오브젝트 복제
+            // J : Quaternion.identity <- 회전값 0
+            GameObject instance = Instantiate(book, spawnPos, Quaternion.identity);
+            bookList.Add(instance); // J : 오브젝트 관리를 위해 리스트에 add
+        }
+        area.enabled = false;       // J : BoxCollider2D 끄기
+        yield return new WaitForSeconds(gameManager.day);   // J : 하루 지남
+
+        for (int i = 0; i < count; i++) // J : 책 삭제
+            Destroy(bookList[i].gameObject);
+
+        bookList.Clear();           // J : bookList 비우기
+        area.enabled = true;        // J : BoxCollider2D 켜기
+        StartCoroutine("Spawn");    // J : 책 다시 스폰
     }
 
     // J : 맵 내의 랜덤한 위치를 return

c17f2e869d13017a04a742fd71c85d7d9981f5fb
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2021-11-27 22:48:54 +0900

    update : 책을 찾았습니다 대화창 뜨지 않게 하기

--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -171,7 +171,7 @@ public class PlayerAction : MonoBehaviour
         if (coll.gameObject.name == "Book(Clone)") {        // J : 부딪힌 오브젝트가 책인 경우
             coll.gameObject.SetActive(false);               // J : Book Object 비활성화
             // manager.talkPanel.SetActive(true);              // J : 대화창 활성화
-            manager.talkText.text = "책을 찾았습니다!";     // J : 대화창 텍스트 적용
+            // manager.talkText.text = "책을 찾았습니다!";     // J : 대화창 텍스트 적용
             screenManager.getBook();                        // J : 책 개수 증가
 
             // C :

a99d8ddfe804090b711abc874d2b1dfc70eb6363
hyeweon <berry_na@khu.ac.kr>
2021-11-27 23:44:35 +0900

    알림창 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -82,7 +82,8 @@ public class GameManager : MonoBehaviour
         }
         Talk(talkId);                   // C : 필요한 talkPanel text 값 가져오기, K : 예외처리를 위해 objData.id > talkId로 수정
 
-        talkPanel.SetActive(isTPShow);      // C : talkPanel 숨기거나 보여주기
+        if(objData.id == 1000) talkPanel.SetActive(isTPShow);      // C : talkPanel 숨기거나 보여주기
+        else GameObject.Find("Alert").transform.Find("Alert Set").gameObject.SetActive(isTPShow); // N : 알림창 띄워주기
     }
 
     // C : 상황에 따라 적절하게 필요한 talkPanel text 값 대화창에 띄우기

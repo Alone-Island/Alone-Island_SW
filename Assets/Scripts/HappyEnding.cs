952b42b336ad303b4c046abcf255b3852bf63f12
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2021-11-28 14:57:49 +0900

    update : happyending

--- Assets/Scripts/HappyEnding.cs
+++ Assets/Scripts/HappyEnding.cs
@@ -5,35 +5,17 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
 
-// K : 텍스트에 효과를 주기 위한 매니저입니다.
-public class TextManager : MonoBehaviour
+public class HappyEnding : MonoBehaviour
 {
     [SerializeField] public TextMeshProUGUI dialog; // K : text 오브젝트를 받아오기 위한 변수입니다. > using TMPro;          
 
-    public string[] fullText;
-
     // K : synopsys의 텍스트들(여러 문장)의 배열입니다.
-    public string[] synopsysFullText = { 
-        "나는 로봇공학자 K...\nAI 로봇을 개발하기 위해\n연구실에서만 지낸 시간이 수년... ", 
-        "드디어... 완성했다... ",
-        "나의 역작, AI 로봇 NJ-C!!!! ",
-        "이제 세상에 공개할 때가 왔...",
-        "아니...뭐지? ",
-        "연구실 밖의 상황을 살펴보니… ",
-        "내가 살고 있던 마을은 섬이 되어었다...",
-        "통신은 모두 끊겨있고..\n사람들은 전혀 보이지 않는다!! ",
-        "갑자기 무인도에 혼자 남게 되다니?!? ",
-        "이제 식량도 거의 다 떨어지고...\n이렇게 죽는건가... ",
-        "하지만 나에게는 NJ-C가 있어! ",
-        "아직 깡통에 불과하는 이 로봇을 학습시켜서\n이 섬에서 살아남아 보자! "
-    };
-
     public string[] HappyEndingFullText = {
-        "박사님!! \n제가 통신기를 만들었어요!!!",
-        "통신기??",
-        "사실 매일 조금씩 만들고 있었는데... \n드디어 완성이에요!",
-        "NJ-C! 당장 통신기를 사용해보자!!",
-        "치...지직....치지지..직...여기는...",
+        "박사님!!\n 제가 통신기를 만들었어요!!",
+        "통신기?",
+        "사실 매일 조금씩 만들고 있었는데...\n드디어 완성이에요!",
+        "NJ-C! 당장 통신기를 사용해보자!",
+        "치...지직....치지지..직...여기는",
     };
 
 
@@ -43,12 +25,11 @@ public class TextManager : MonoBehaviour
     public bool isTyping = true; // K : 현재 글자가 화면에 타이핑되고 있는지 확인하기 위한 변수입니다.
     bool isSkipPart = false;
 
-    void Start() {
-        fullText = synopsysFullText;
-        fullText = HappyEndingFullText;
+    void Start()
+    {
         StartCoroutine("TypingAction", 0);          // K : 스크립트의 시작과 동시에 타이핑을 시작하는 코드입니다.
     }
-    
+
     void Update()
     {
         if (Input.GetKeyUp(KeyCode.Space))    // K : 스페이스바를 눌렀을 때
@@ -56,7 +37,7 @@ public class TextManager : MonoBehaviour
             if (!isTyping)  // K : 현재 글자가 화면에 타이핑 되고 있지 않을 때
             {
                 // K : 스페이스바를 눌렀을 때 현재 글자가 화면에 타이핑 되고 있지 않는다면, 다음 문장 타이핑하기 위한 코드
-                StartCoroutine("TypingAction", 0); 
+                StartCoroutine("TypingAction", 0);
             }
             else
             {
@@ -77,27 +58,28 @@ public class TextManager : MonoBehaviour
         SceneManager.LoadScene(screenName); // K : 게임 신으로 가게 하는 함수입니다. > using UnityEngine.SceneManagement;
     }
 
-    IEnumerator TypingAction() {
-        if (currentPoint >= fullText.Length)    // K : 모든 텍스트들을 타이핑 했을 때
+    IEnumerator TypingAction()
+    {
+        if (currentPoint >= HappyEndingFullText.Length)    // K : 모든 텍스트들을 타이핑 했을 때
         {
             Debug.Log("Game Start"); // K : 모든 텍스트를 출력 완료, 게임 플레이 신으로 이동
-            GoToGameScreen("MainGame");
+            GoToGameScreen("EndingCredits");
         }
 
         dialog.text = "";   // K : Text 오브젝트의 text 초기화
         isTyping = true;    // K : 텍스트 화면에 타이핑을 시작했기 때문에, isTyping true
 
-        for (int i = 0; i < fullText[currentPoint].Length; i++) // K : 텍스트 한 문장의 한 글자 한 글자를 화면에 나타나게 하기 위한 반복문
+        for (int i = 0; i < HappyEndingFullText[currentPoint].Length; i++) // K : 텍스트 한 문장의 한 글자 한 글자를 화면에 나타나게 하기 위한 반복문
         {
             yield return new WaitForSeconds(0.15f); // K : 텍스트 한 글자 한 글자 사이의 딜레이
 
-            subText += fullText[currentPoint].Substring(0, i);  // K : 텍스트의 인덱스 0~i까지 자름
+            subText += HappyEndingFullText[currentPoint].Substring(0, i);  // K : 텍스트의 인덱스 0~i까지 자름
             dialog.text = subText;                                      // K : Text 오브젝트에 subText 적용 
             subText = "";                                               // K : subText 초기화
 
             if (isSkipPart)                                             // K : 부분 스킵이 true면
             {
-                dialog.text = fullText[currentPoint];           // K : Text 오브젝트에 전체 텍스트 문장을 띄운다
+                dialog.text = HappyEndingFullText[currentPoint];           // K : Text 오브젝트에 전체 텍스트 문장을 띄운다
                 isSkipPart = false;                                     // K : isSkipPart 초기화
                 isTyping = false;                                       // K : isTyping 초기화
                 break;

691abe6ea91e0b9eb7907e15d4f9f5a314b80bd5
Jangmanbo <wkddpdnjs99@gmail.com>
2021-12-01 04:59:36 +0900

    clear

--- Assets/Scripts/ObjectData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-// C : Object Layer들을 관리하는 스크립트
-public class ObjectData : MonoBehaviour
-{
-    public int id;      // Object를 구분하기 위한 id
-}

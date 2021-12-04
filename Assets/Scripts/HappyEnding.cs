333af6ef151bb9b4ec8e5715528c7a796e0b411b
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2021-12-04 12:14:25 +0900

    add: comments to HappyEnding.cs

--- Assets/Scripts/HappyEnding.cs
+++ Assets/Scripts/HappyEnding.cs
@@ -10,12 +10,12 @@ public class HappyEnding : MonoBehaviour
     public GameObject panel;            // N : 화면 어둡게
 
     [SerializeField] public TextMeshProUGUI dialog; // K : text 오브젝트를 받아오기 위한 변수입니다. > using TMPro;          
-    public string[] fullText;
+    public string[] fullText;                       // K : 타이핑 효과를 줄 텍스트를 담기 위한 배열입니다
 
-    private int endingCode;
+    private int endingCode;                         // K : 해피 엔딩 코드를 담을 변수 입니다.
 
     // K : synopsys의 텍스트들(여러 문장)의 배열입니다.
-    
+
     private string[] happySosoLifeFullText = {
         "벌써 90일이 지났어요..",
         "이런 하루하루도 나쁘지는 않네요..",
@@ -44,14 +44,14 @@ public class HappyEnding : MonoBehaviour
     int currentPoint = 0; // K : synopsysFullText에서 현재 포인터가 어디있는지 저장하기 위한 변수입니다.
 
     public bool isTyping = true; // K : 현재 글자가 화면에 타이핑되고 있는지 확인하기 위한 변수입니다.
-    bool isSkipPart = false;
-    bool isEnd = false;
-    bool isEndingCardShow = false;
+    bool isSkipPart = false;     // K : 현재 문장을 타이핑 효과 없이 바로 화면에 띄울지 확인하기 위한 변수입니다.
+    bool isEnd = false;                 // K : 해피엔딩 카드가 뜨고 일정시간이 지난 후 해피엔딩 신이 종료했느닞 확인하기 위한 변수입니다.
+    bool isEndingCardShow = false;      // K : 해피엔딩 카드가 화면에 띄워졌는지 확인하기 위한 변수입니다.
 
     void Start()
     {
-        endingCode = DataController.Instance.endingData.currentEndingCode;
-        switch (endingCode)
+        endingCode = DataController.Instance.endingData.currentEndingCode; // K : DataController 중 eningData에 저장된 현재 엔딩코드를 가져옵니다.
+        switch (endingCode) // K : 엔딩코드에 따라 맞는 엔딩 스크립트를 가져옵니다.
         {
             case 101:
                 fullText = happySosoLifeFullText;
@@ -76,20 +76,20 @@ public class HappyEnding : MonoBehaviour
     {
         if (Input.GetKeyUp(KeyCode.Space))    // K : 스페이스바를 눌렀을 때
         {
-            if (!isEndingCardShow)
+            if (!isEndingCardShow)  // K : 해피 엔딩 카드가 보여지고 있지 않을때만 타이핑 효과를 주기 위한 코드입니다.
             {
                 if (!isTyping)  // K : 현재 글자가 화면에 타이핑 되고 있지 않을 때
                 {
-                    // K : 스페이스바를 눌렀을 때 현재 글자가 화면에 타이핑 되고 있지 않는다면, 다음 문장 타이핑하기 위한 코드
+                    // K : 스페이스바를 눌렀을 때 현재 글자가 화면에 타이핑 되고 있지 않는다면, 다음 문장 타이핑하기 위한 코드입니다.
                     StartCoroutine("TypingAction", 0);
                 }
                 else
                 {
-                    // K : 스페이스바를 눌렀을 때 현재 글자가 화면에 타이핑 되고 있다면, 부분 스킵을 위해 isSkipPart true로 변경하는 코드
+                    // K : 스페이스바를 눌렀을 때 현재 글자가 화면에 타이핑 되고 있다면, 부분 스킵을 위해 isSkipPart true로 변경하는 코드입니다.
                     isSkipPart = true;
                 }
             } 
-            if (isEnd)
+            if (isEnd) // K : 해피엔딩이 종료되었을 때만 엔딩 크레딧 신으로 넘어가기 위한 코드입니다.
             {
                 SceneManager.LoadScene("EndingCredits");
             }
@@ -100,7 +100,7 @@ public class HappyEnding : MonoBehaviour
     {
         if (currentPoint >= fullText.Length)    // K : 모든 텍스트들을 타이핑 했을 때
         {           
-            ShowHappyEndingCard();
+            ShowHappyEndingCard();              // K : 모든 텍스트가 타이핑이 종료하면 해피엔딩 카드를 띄우는 함수를 호출합니다.
             StopCoroutine("TypingAction");
             yield return 0;
         }
@@ -134,28 +134,30 @@ public class HappyEnding : MonoBehaviour
     // N : 스페이스바 입력 받는 시간을 주기 위해
     public void TheEnd()
     {
-        isEnd = true;
-        DataController.Instance.endingData.currentEndingCode = 0;
+        isEnd = true;                                             // K : 해피 엔딩 신이 종료됨
+        DataController.Instance.endingData.currentEndingCode = 0; // K : DataController 중 eningData에 저장된 현재 엔딩코드를 초기화 합니다.
     }
 
     // J : 모든 엔딩의 공통 코드
     private void ending()
-    {        
-        panel.SetActive(true);
-        DataController.Instance.settingData.firstGame = 0;
+    {
+        Debug.Log("Game Happy Ending Credits");
+        currentPoint = 0;               // K : 타이핑 효과를 준 배열의 포인터를 0으로 초기화
+        subText = "";                   // K : 타이핑 효과를 주기 위해 텍스트의 일부 를 담기 위한 변수 초기화
+        isTyping = false;               // K : 타이핑이 멈춤
+        isSkipPart = false;             // K : 타이핑 스킵 초기화
+
+        isEndingCardShow = true;        // K : 엔딩 카드가 띄워졌음
+
+        panel.SetActive(true);                                  // K : 배경을 어둡게 하기위한 패널을 띄웁니다
+        DataController.Instance.settingData.firstGame = 0;      // K : 첫번째 게임이 아님을 확인하기 위해 firstGame을 0으로 변경합니다.
     }
 
 
     // K : 해피 엔딩 endingCode 101-104
     public void ShowHappyEndingCard()
-    {
-        Debug.Log("Game Happy Ending Credits");
-        currentPoint = 0;
-        subText = "";
-        isTyping = false;
-        isSkipPart = false;
-        isEndingCardShow = true;
-        ending();
+    {        
+        ending();                       // K : 공통 엔딩 코드 함수 호출
         switch (endingCode)
         {
             case 101:
@@ -174,6 +176,6 @@ public class HappyEnding : MonoBehaviour
                 Debug.Log("해피엔딩 카드 에러");
                 break;
         }
-        Invoke("TheEnd", 2.0f);
+        Invoke("TheEnd", 2.0f);           // K : 카드를 띄운 뒤 2초후에 해피 엔딩 신 종료 함수 호출
     }
 }

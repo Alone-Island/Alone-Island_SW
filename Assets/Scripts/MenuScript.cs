4dafe6bd763a17996f55f9913643a35aa74388e5
Jangmanbo <wkddpdnjs99@gmail.com>
2022-01-11 04:06:10 +0900

    add : ending card detail in menu
    엔딩카드창에서 카드를 클릭하면 자세한 내용 볼 수 있음

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -10,12 +10,13 @@ public class MenuScript : MonoBehaviour
     public GameObject scrollView;   // J : 엔딩카드창
 
     public GameObject BGM;          // J : 배경음악 on/off를 위해 BGM object 가져옴
-    public GameObject onImage;      // J : BGM on 이미지
-    public GameObject offImage;     // J : BGM off 이미지
 
-    public GameObject endingCards;  // J : Scroll View->Viewport->Content
+    public GameObject CardButton;  // J : Windows->CardButton
+    public GameObject scrollEndingCards;  // J : Scroll View->Viewport->Content
     public Sprite badCard;
     public Sprite happyCard;
+
+    private GameObject currentCard;
 
 
     // J : 시작하기 버튼 onclick
@@ -26,121 +27,6 @@ public class MenuScript : MonoBehaviour
             SceneManager.LoadScene("Synopsis"); // J : Synopsis scene으로 이동
         else
             SceneManager.LoadScene("MainGame"); // J : MainGame scene으로 이동
-    }
-
-    // J : 엔딩카드 버튼 onclick
-    public void SelectCard()
-    {
-        Debug.Log("엔딩카드");
-        scrollView.SetActive(true);
-
-        Image card;
-
-        // J : BadLine0 엔딩카드
-        if (DataController.Instance.endingData.hungry == 1)
-        {
-            card = endingCards.transform.Find("BadLine0").transform.Find("hungry").GetComponent<Image>();
-            card.sprite = badCard;
-            endingCards.transform.Find("BadLine0").transform.Find("hungry").transform.Find("Image").gameObject.SetActive(false);
-            endingCards.transform.Find("BadLine0").transform.Find("hungry").transform.Find("hungry").gameObject.SetActive(true);
-        }
-        if (DataController.Instance.endingData.lonely == 1)
-        {
-            card = endingCards.transform.Find("BadLine0").transform.Find("lonely").GetComponent<Image>();
-            card.sprite = badCard;
-            endingCards.transform.Find("BadLine0").transform.Find("lonely").transform.Find("Image").gameObject.SetActive(false);
-            endingCards.transform.Find("BadLine0").transform.Find("lonely").transform.Find("lonely").gameObject.SetActive(true);
-        }
-        if (DataController.Instance.endingData.cold == 1)
-        {
-            card = endingCards.transform.Find("BadLine0").transform.Find("cold").GetComponent<Image>();
-            card.sprite = badCard;
-            endingCards.transform.Find("BadLine0").transform.Find("cold").transform.Find("Image").gameObject.SetActive(false);
-            endingCards.transform.Find("BadLine0").transform.Find("cold").transform.Find("cold").gameObject.SetActive(true);
-        }
-
-        // J : BadLine1 엔딩카드
-        if (DataController.Instance.endingData.poisonBerry == 1)
-        {
-            card = endingCards.transform.Find("BadLine1").transform.Find("poisonBerry").GetComponent<Image>();
-            card.sprite = badCard;
-            endingCards.transform.Find("BadLine1").transform.Find("poisonBerry").transform.Find("Image").gameObject.SetActive(false);
-            endingCards.transform.Find("BadLine1").transform.Find("poisonBerry").transform.Find("poisonBerry").gameObject.SetActive(true);
-        }
-        if (DataController.Instance.endingData.error == 1)
-        {
-            card = endingCards.transform.Find("BadLine1").transform.Find("error").GetComponent<Image>();
-            card.sprite = badCard;
-            endingCards.transform.Find("BadLine1").transform.Find("error").transform.Find("Image").gameObject.SetActive(false);
-            endingCards.transform.Find("BadLine1").transform.Find("error").transform.Find("error").gameObject.SetActive(true);
-        }
-        if (DataController.Instance.endingData.electric == 1)
-        {
-            card = endingCards.transform.Find("BadLine1").transform.Find("electric").GetComponent<Image>();
-            card.sprite = badCard;
-            endingCards.transform.Find("BadLine1").transform.Find("electric").transform.Find("Image").gameObject.SetActive(false);
-            endingCards.transform.Find("BadLine1").transform.Find("electric").transform.Find("electric").gameObject.SetActive(true);
-        }
-        if (DataController.Instance.endingData.pig == 1)
-        {
-            card = endingCards.transform.Find("BadLine1").transform.Find("pig").GetComponent<Image>();
-            card.sprite = badCard;
-            endingCards.transform.Find("BadLine1").transform.Find("pig").transform.Find("Image").gameObject.SetActive(false);
-            endingCards.transform.Find("BadLine1").transform.Find("pig").transform.Find("pig").gameObject.SetActive(true);
-        }
-        if (DataController.Instance.endingData.storm == 1)
-        {
-            card = endingCards.transform.Find("BadLine1").transform.Find("storm").GetComponent<Image>();
-            card.sprite = badCard;
-            endingCards.transform.Find("BadLine1").transform.Find("storm").transform.Find("Image").gameObject.SetActive(false);
-            endingCards.transform.Find("BadLine1").transform.Find("storm").transform.Find("storm").gameObject.SetActive(true);
-        }
-
-        // J : BadLine2 엔딩카드
-        if (DataController.Instance.endingData.space == 1)
-        {
-            card = endingCards.transform.Find("BadLine2").transform.Find("space").GetComponent<Image>();
-            card.sprite = badCard;
-            endingCards.transform.Find("BadLine2").transform.Find("space").transform.Find("Image").gameObject.SetActive(false);
-            endingCards.transform.Find("BadLine2").transform.Find("space").transform.Find("space").gameObject.SetActive(true);
-        }
-
-        // J : HappyLine0 엔딩카드
-        if (DataController.Instance.endingData.timeOut == 1)
-        {
-            card = endingCards.transform.Find("HappyLine0").transform.Find("timeOut").GetComponent<Image>();
-            card.sprite = happyCard;
-            endingCards.transform.Find("HappyLine0").transform.Find("timeOut").transform.Find("Image").gameObject.SetActive(false);
-            endingCards.transform.Find("HappyLine0").transform.Find("timeOut").transform.Find("timeOut").gameObject.SetActive(true);
-        }
-        if (DataController.Instance.endingData.two == 1)
-        {
-            card = endingCards.transform.Find("HappyLine0").transform.Find("two").GetComponent<Image>();
-            card.sprite = happyCard;
-            endingCards.transform.Find("HappyLine0").transform.Find("two").transform.Find("Image").gameObject.SetActive(false);
-            endingCards.transform.Find("HappyLine0").transform.Find("two").transform.Find("two").gameObject.SetActive(true);
-        }
-        if (DataController.Instance.endingData.AITown == 1)
-        {
-            card = endingCards.transform.Find("HappyLine0").transform.Find("AITown").GetComponent<Image>();
-            card.sprite = happyCard;
-            endingCards.transform.Find("HappyLine0").transform.Find("AITown").transform.Find("Image").gameObject.SetActive(false);
-            endingCards.transform.Find("HappyLine0").transform.Find("AITown").transform.Find("AITown").gameObject.SetActive(true);
-        }
-        if (DataController.Instance.endingData.people == 1)
-        {
-            card = endingCards.transform.Find("HappyLine0").transform.Find("people").GetComponent<Image>();
-            card.sprite = happyCard;
-            endingCards.transform.Find("HappyLine0").transform.Find("people").transform.Find("Image").gameObject.SetActive(false);
-            endingCards.transform.Find("HappyLine0").transform.Find("people").transform.Find("people").gameObject.SetActive(true);
-        }
-    }
-
-    // J : 엔딩카드 나가기 버튼 onclick
-    public void SelectCardQuit()
-    {
-        Debug.Log("엔딩카드 나가기");
-        scrollView.SetActive(false);  // J : 게임방법창 비활성화
     }
 
     // J : 게임방법 버튼 onclick
@@ -163,4 +49,258 @@ public class MenuScript : MonoBehaviour
         Debug.Log("게임종료");
         Application.Quit(); // J : 프로그램 종료
     }
+
+    // J : 엔딩카드 버튼 onclick
+    public void SelectEndingCard()
+    {
+        Debug.Log("엔딩카드");
+        scrollView.SetActive(true);
+
+        Image card;
+
+        // J : BadLine0 엔딩카드
+        if (DataController.Instance.endingData.hungry == 1)
+        {
+            card = scrollEndingCards.transform.Find("BadLine0").transform.Find("hungry").GetComponent<Image>();
+            card.sprite = badCard;
+            scrollEndingCards.transform.Find("BadLine0").transform.Find("hungry").transform.Find("Image").gameObject.SetActive(false);
+            scrollEndingCards.transform.Find("BadLine0").transform.Find("hungry").transform.Find("hungry").gameObject.SetActive(true);
+        }
+        if (DataController.Instance.endingData.lonely == 1)
+        {
+            card = scrollEndingCards.transform.Find("BadLine0").transform.Find("lonely").GetComponent<Image>();
+            card.sprite = badCard;
+            scrollEndingCards.transform.Find("BadLine0").transform.Find("lonely").transform.Find("Image").gameObject.SetActive(false);
+            scrollEndingCards.transform.Find("BadLine0").transform.Find("lonely").transform.Find("lonely").gameObject.SetActive(true);
+        }
+        if (DataController.Instance.endingData.cold == 1)
+        {
+            card = scrollEndingCards.transform.Find("BadLine0").transform.Find("cold").GetComponent<Image>();
+            card.sprite = badCard;
+            scrollEndingCards.transform.Find("BadLine0").transform.Find("cold").transform.Find("Image").gameObject.SetActive(false);
+            scrollEndingCards.transform.Find("BadLine0").transform.Find("cold").transform.Find("cold").gameObject.SetActive(true);
+        }
+
+        // J : BadLine1 엔딩카드
+        if (DataController.Instance.endingData.poisonBerry == 1)
+        {
+            card = scrollEndingCards.transform.Find("BadLine1").transform.Find("poisonBerry").GetComponent<Image>();
+            card.sprite = badCard;
+            scrollEndingCards.transform.Find("BadLine1").transform.Find("poisonBerry").transform.Find("Image").gameObject.SetActive(false);
+            scrollEndingCards.transform.Find("BadLine1").transform.Find("poisonBerry").transform.Find("poisonBerry").gameObject.SetActive(true);
+        }
+        if (DataController.Instance.endingData.error == 1)
+        {
+            card = scrollEndingCards.transform.Find("BadLine1").transform.Find("error").GetComponent<Image>();
+            card.sprite = badCard;
+            scrollEndingCards.transform.Find("BadLine1").transform.Find("error").transform.Find("Image").gameObject.SetActive(false);
+            scrollEndingCards.transform.Find("BadLine1").transform.Find("error").transform.Find("error").gameObject.SetActive(true);
+        }
+        if (DataController.Instance.endingData.electric == 1)
+        {
+            card = scrollEndingCards.transform.Find("BadLine1").transform.Find("electric").GetComponent<Image>();
+            card.sprite = badCard;
+            scrollEndingCards.transform.Find("BadLine1").transform.Find("electric").transform.Find("Image").gameObject.SetActive(false);
+            scrollEndingCards.transform.Find("BadLine1").transform.Find("electric").transform.Find("electric").gameObject.SetActive(true);
+        }
+        if (DataController.Instance.endingData.pig == 1)
+        {
+            card = scrollEndingCards.transform.Find("BadLine1").transform.Find("pig").GetComponent<Image>();
+            card.sprite = badCard;
+            scrollEndingCards.transform.Find("BadLine1").transform.Find("pig").transform.Find("Image").gameObject.SetActive(false);
+            scrollEndingCards.transform.Find("BadLine1").transform.Find("pig").transform.Find("pig").gameObject.SetActive(true);
+        }
+        if (DataController.Instance.endingData.storm == 1)
+        {
+            card = scrollEndingCards.transform.Find("BadLine1").transform.Find("storm").GetComponent<Image>();
+            card.sprite = badCard;
+            scrollEndingCards.transform.Find("BadLine1").transform.Find("storm").transform.Find("Image").gameObject.SetActive(false);
+            scrollEndingCards.transform.Find("BadLine1").transform.Find("storm").transform.Find("storm").gameObject.SetActive(true);
+        }
+
+        // J : BadLine2 엔딩카드
+        if (DataController.Instance.endingData.space == 1)
+        {
+            card = scrollEndingCards.transform.Find("BadLine2").transform.Find("space").GetComponent<Image>();
+            card.sprite = badCard;
+            scrollEndingCards.transform.Find("BadLine2").transform.Find("space").transform.Find("Image").gameObject.SetActive(false);
+            scrollEndingCards.transform.Find("BadLine2").transform.Find("space").transform.Find("space").gameObject.SetActive(true);
+        }
+
+        // J : HappyLine0 엔딩카드
+        if (DataController.Instance.endingData.timeOut == 1)
+        {
+            card = scrollEndingCards.transform.Find("HappyLine0").transform.Find("timeOut").GetComponent<Image>();
+            card.sprite = happyCard;
+            scrollEndingCards.transform.Find("HappyLine0").transform.Find("timeOut").transform.Find("Image").gameObject.SetActive(false);
+            scrollEndingCards.transform.Find("HappyLine0").transform.Find("timeOut").transform.Find("timeOut").gameObject.SetActive(true);
+        }
+        if (DataController.Instance.endingData.two == 1)
+        {
+            card = scrollEndingCards.transform.Find("HappyLine0").transform.Find("two").GetComponent<Image>();
+            card.sprite = happyCard;
+            scrollEndingCards.transform.Find("HappyLine0").transform.Find("two").transform.Find("Image").gameObject.SetActive(false);
+            scrollEndingCards.transform.Find("HappyLine0").transform.Find("two").transform.Find("two").gameObject.SetActive(true);
+        }
+        if (DataController.Instance.endingData.AITown == 1)
+        {
+            card = scrollEndingCards.transform.Find("HappyLine0").transform.Find("AITown").GetComponent<Image>();
+            card.sprite = happyCard;
+            scrollEndingCards.transform.Find("HappyLine0").transform.Find("AITown").transform.Find("Image").gameObject.SetActive(false);
+            scrollEndingCards.transform.Find("HappyLine0").transform.Find("AITown").transform.Find("AITown").gameObject.SetActive(true);
+        }
+        if (DataController.Instance.endingData.people == 1)
+        {
+            card = scrollEndingCards.transform.Find("HappyLine0").transform.Find("people").GetComponent<Image>();
+            card.sprite = happyCard;
+            scrollEndingCards.transform.Find("HappyLine0").transform.Find("people").transform.Find("Image").gameObject.SetActive(false);
+            scrollEndingCards.transform.Find("HappyLine0").transform.Find("people").transform.Find("people").gameObject.SetActive(true);
+        }
+    }
+
+    // J : 스크롤뷰에서 배고픔 카드 클릭
+    public void BadLine00()
+    {
+        if (DataController.Instance.endingData.hungry == 1)
+        {
+            currentCard = CardButton.transform.Find("Bad-Hungry").gameObject;
+            currentCard.SetActive(true);
+        }
+    }
+
+    // J : 스크롤뷰에서 외로움 카드 클릭
+    public void BadLine01()
+    {
+        if (DataController.Instance.endingData.lonely == 1)
+        {
+            currentCard = CardButton.transform.Find("Bad-Lonely").gameObject;
+            currentCard.SetActive(true);
+        }
+    }
+
+    // J : 스크롤뷰에서 추움 카드 클릭
+    public void BadLine02()
+    {
+        if (DataController.Instance.endingData.cold == 1)
+        {
+            currentCard = CardButton.transform.Find("Bad-Frozen").gameObject;
+            currentCard.SetActive(true);
+        }
+    }
+
+    // J : 스크롤뷰에서 독열매 카드 클릭
+    public void BadLine10()
+    {
+        if (DataController.Instance.endingData.poisonBerry == 1)
+        {
+            currentCard = CardButton.transform.Find("Bad-Berry").gameObject;
+            currentCard.SetActive(true);
+        }
+    }
+
+    // J : 스크롤뷰에서 에러 카드 클릭
+    public void BadLine11()
+    {
+        if (DataController.Instance.endingData.error == 1)
+        {
+            currentCard = CardButton.transform.Find("Bad-Error").gameObject;
+            currentCard.SetActive(true);
+        }
+    }
+
+    // J : 스크롤뷰에서 감전사 카드 클릭
+    public void BadLine12()
+    {
+        if (DataController.Instance.endingData.electric == 1)
+        {
+            currentCard = CardButton.transform.Find("Bad-Electric").gameObject;
+            currentCard.SetActive(true);
+        }
+    }
+
+    // J : 스크롤뷰에서 멧돼지 카드 클릭
+    public void BadLine13()
+    {
+        if (DataController.Instance.endingData.pig == 1)
+        {
+            currentCard = CardButton.transform.Find("Bad-Pig").gameObject;
+            currentCard.SetActive(true);
+        }
+    }
+
+    // J : 스크롤뷰에서 폭풍우 카드 클릭
+    public void BadLine14()
+    {
+        if (DataController.Instance.endingData.storm == 1)
+        {
+            currentCard = CardButton.transform.Find("Bad-Storm").gameObject;
+            currentCard.SetActive(true);
+        }
+    }
+
+    // J : 스크롤뷰에서 운석충돌 카드 클릭
+    public void BadLine20()
+    {
+        if (DataController.Instance.endingData.space == 1)
+        {
+            currentCard = CardButton.transform.Find("Bad-Space").gameObject;
+            currentCard.SetActive(true);
+        }
+    }
+
+    // J : 스크롤뷰에서 그럭저럭 카드 클릭
+    public void HappyLine00()
+    {
+        if (DataController.Instance.endingData.timeOut == 1)
+        {
+            currentCard = CardButton.transform.Find("Happy-SosoLife").gameObject;
+            currentCard.SetActive(true);
+        }
+    }
+
+    // J : 스크롤뷰에서 둘이서 행복 카드 클릭
+    public void HappyLine01()
+    {
+        if (DataController.Instance.endingData.two == 1)
+        {
+            currentCard = CardButton.transform.Find("Happy-Two").gameObject;
+            currentCard.SetActive(true);
+        }
+    }
+
+    // J : 스크롤뷰에서 AI Town 카드 클릭
+    public void HappyLine02()
+    {
+        if (DataController.Instance.endingData.AITown == 1)
+        {
+            currentCard = CardButton.transform.Find("Happy-AITown").gameObject;
+            currentCard.SetActive(true);
+        }
+    }
+
+    // J : 스크롤뷰에서 통신 카드 클릭
+    public void HappyLine03()
+    {
+        if (DataController.Instance.endingData.people == 1)
+        {
+            currentCard = CardButton.transform.Find("Happy-People").gameObject;
+            currentCard.SetActive(true);
+        }
+    }
+
+    // J : 현재 보이는 카드 클릭
+    public void SelectCurrentCard()
+    {
+        currentCard.SetActive(false);   // J : 카드 비활성화
+    }
+
+    // J : 엔딩카드 나가기 버튼 onclick
+    public void SelectEndingCardQuit()
+    {
+        Debug.Log("엔딩카드 나가기");
+        if (currentCard != null)
+            currentCard.SetActive(false);
+        scrollView.SetActive(false);  // J : 게임방법창 비활성화
+    }
+
 }

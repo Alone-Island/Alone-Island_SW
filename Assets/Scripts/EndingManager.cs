ca2ec3f5b1cab7f4d51006cf4977f653cf090fbe
Jangmanbo <wkddpdnjs99@gmail.com>
2022-01-13 05:40:46 +0900

    add : save json file when game end

--- Assets/Scripts/EndingManager.cs
+++ Assets/Scripts/EndingManager.cs
@@ -117,6 +117,10 @@ public class EndingManager : MonoBehaviour
         }
         effect.Play("BadEndingEffect");
         panel.SetActive(true);
+
+        // J : 데이터 파일 저장
+        DataController.Instance.SaveSettingData();
+        DataController.Instance.SaveEndingData();
         Invoke("TheEnd", 2.0f);
     }
 
@@ -124,10 +128,14 @@ public class EndingManager : MonoBehaviour
 
     // K : 해피 엔딩
     public void timeOutEnding()
-    {        
+    {
+        Debug.Log("그냥 저냥 살았습니당 ~~");
         DataController.Instance.endingData.timeOut = 1;
         DataController.Instance.endingData.currentEndingCode = 101;
-        Debug.Log("그냥 저냥 살았습니당 ~~");
+        // J : 데이터 파일 저장
+        DataController.Instance.SaveSettingData();
+        DataController.Instance.SaveEndingData();
+
         fadeManager.GameFadeOut(LoadHaapyScene); // J : 페이드아웃
     }
 
@@ -136,6 +144,10 @@ public class EndingManager : MonoBehaviour
         Debug.Log("human과 ai는 단둘이 행복하게 살았답니다");
         DataController.Instance.endingData.two = 1;
         DataController.Instance.endingData.currentEndingCode = 102;
+        // J : 데이터 파일 저장
+        DataController.Instance.SaveSettingData();
+        DataController.Instance.SaveEndingData();
+
         fadeManager.GameFadeOut(LoadHaapyScene); // J : 페이드아웃
     }
 
@@ -144,6 +156,10 @@ public class EndingManager : MonoBehaviour
         Debug.Log("다른 ai를 만들어내서 ai들과 함께 살게 됨");
         DataController.Instance.endingData.AITown = 1;
         DataController.Instance.endingData.currentEndingCode = 103;
+        // J : 데이터 파일 저장
+        DataController.Instance.SaveSettingData();
+        DataController.Instance.SaveEndingData();
+
         fadeManager.GameFadeOut(LoadHaapyScene); // J : 페이드아웃
     }
 
@@ -152,6 +168,10 @@ public class EndingManager : MonoBehaviour
         Debug.Log("통신기를 만들어서 다른 생존자들을 만남");
         DataController.Instance.endingData.people = 1;
         DataController.Instance.endingData.currentEndingCode = 104;
+        // J : 데이터 파일 저장
+        DataController.Instance.SaveSettingData();
+        DataController.Instance.SaveEndingData();
+
         fadeManager.GameFadeOut(LoadHaapyScene); // J : 페이드아웃
     }
 

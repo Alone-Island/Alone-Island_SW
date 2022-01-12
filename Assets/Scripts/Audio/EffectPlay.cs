d87a4a36009b18a12affcd28131fb80da3c1768b
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-01-12 23:38:16 +0900

    update : effect dondestroyonload

--- Assets/Scripts/Audio/EffectPlay.cs
+++ Assets/Scripts/Audio/EffectPlay.cs
@@ -7,6 +7,11 @@ public class EffectPlay : MonoBehaviour
     private AudioSource audioSource;
     private bool effectOn = false;
 
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
